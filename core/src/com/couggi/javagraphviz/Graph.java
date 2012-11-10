17da6da485d51d86ed94e2d50f12b2eb4f769220
Mateusz Parzonka <parzonka@gmail.com>
2012-11-10 23:07:24 +0100

    Apply formatting

--- core/src/com/couggi/javagraphviz/Graph.java
+++ core/src/com/couggi/javagraphviz/Graph.java
@@ -6,44 +6,44 @@ import java.util.List;
  * type Graph component of the graphviz tools.
  * 
  * @author Everton Cardoso
- *
+ * 
  */
 public interface Graph extends Component {
 
-	/**
-	 * attributes default of the nodes.
-	 */
-	Node getDefaultNode();
-	
-	/**
-	 * attributes default of the edges.
-	 */
-	Edge edge();
-	
-	/**
-	 * list of the nodes.
-	 */
-	List<Node> nodes();
-	
-	/**
-	 * list of the edges.
-	 */
-	List<Edge> edges();
-	
-	/**
-	 * type of the Graph componenet (graph, digraph.. etc)
-	 */
-	String getType();
-	
-	/**
-	 * create a node with name to graph.
-	 */
-	Node addNode(String name);
-	
-	/**
-	 * create a edge with two nodes.
-	 */
-	Edge addEdge(Node nodeFrom, Node nodeTo);
-	
-	boolean containsNode(Node node);
+    /**
+     * attributes default of the nodes.
+     */
+    Node getDefaultNode();
+
+    /**
+     * attributes default of the edges.
+     */
+    Edge edge();
+
+    /**
+     * list of the nodes.
+     */
+    List<Node> nodes();
+
+    /**
+     * list of the edges.
+     */
+    List<Edge> edges();
+
+    /**
+     * type of the Graph componenet (graph, digraph.. etc)
+     */
+    String getType();
+
+    /**
+     * create a node with name to graph.
+     */
+    Node addNode(String name);
+
+    /**
+     * create a edge with two nodes.
+     */
+    Edge addEdge(Node nodeFrom, Node nodeTo);
+
+    boolean containsNode(Node node);
 }
