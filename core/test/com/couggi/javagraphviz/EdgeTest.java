d23b0094ecd4e96dd809d33d1721f8f4a4f6110e
Mateusz Parzonka <parzonka@gmail.com>
2012-11-10 23:37:04 +0100

    Apply formatting to EdgeTest

--- core/test/com/couggi/javagraphviz/EdgeTest.java
+++ core/test/com/couggi/javagraphviz/EdgeTest.java
@@ -12,25 +12,24 @@ import com.couggi.javagraphviz.Node;
 
 public class EdgeTest {
 
-	Edge edge;
-	
-	@Before
-	public void onSetUp() { 
-		Graph graph = new Digraph("test");
-		edge = new Edge(new Node("nodeA", graph), new Node("nodeB", graph), graph);
-	}
-	
-	
-	@Test
-	public void testOutput() {
-		
-		edge.attr("shape").value("folder");
-		
-		StringBuffer xAttr = new StringBuffer("");
-		xAttr.append("shape" + " = " + "\"folder\"");
-		StringBuffer xOut = new StringBuffer("nodeA -> nodeB [" + xAttr.toString() + "];");
-		
-		assertEquals(xOut.toString(), edge.output());
-	}
+    Edge edge;
+
+    @Before
+    public void onSetUp() {
+	Graph graph = new Digraph("test");
+	edge = new Edge(new Node("nodeA", graph), new Node("nodeB", graph), graph);
+    }
+
+    @Test
+    public void testOutput() {
+
+	edge.attr("shape").value("folder");
+
+	StringBuffer xAttr = new StringBuffer("");
+	xAttr.append("shape" + " = " + "\"folder\"");
+	StringBuffer xOut = new StringBuffer("nodeA -> nodeB [" + xAttr.toString() + "];");
+
+	assertEquals(xOut.toString(), edge.output());
+    }
 
 }
