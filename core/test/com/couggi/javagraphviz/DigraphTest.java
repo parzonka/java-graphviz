42ed44fb4490f220d3f9034b47f14b6fce55406a
Mateusz Parzonka <parzonka@gmail.com>
2012-11-11 02:10:58 +0100

    Fix DigraphTest

--- core/test/com/couggi/javagraphviz/DigraphTest.java
+++ core/test/com/couggi/javagraphviz/DigraphTest.java
@@ -71,7 +71,7 @@ public class DigraphTest {
 	
 		digraph.setAttribute("bgcolor", "#000");
 		digraph.getDefaultNode().setAttribute("shape", "doublecircle");
-		digraph.getGlobalEdgeAttributes().setAttribute("shape", "folder");
+		digraph.getGlobalEdgeAttributes().put("shape", "folder");
 		Node nodeA = digraph.addNode("nodeA");
 		nodeA.setAttribute("fillcolor", "#fff");
 		Node nodeB = digraph.addNode("nodeB");
